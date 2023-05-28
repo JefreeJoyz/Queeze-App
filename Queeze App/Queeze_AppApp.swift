b9968b8c679ac5b0f054c1677c013a7872f5826a
jefreeHome <jefpad12@gmail.com>
2023-05-28 19:51:35 +0300

    Initial Commit

---
+++ Queeze App/Queeze_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Queeze_AppApp.swift
+//  Queeze App
+//
+//  Created by Yakushev Yevhen on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Queeze_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
